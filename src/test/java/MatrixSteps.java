2b5884cb403f3f1629e2b3e27dcba7964bfc15d5
amira791 <ka_bellali@esi.dz>
2024-01-04 15:06:37 +0100

    test

--- src/test/java/MatrixSteps.java
+++ src/test/java/MatrixSteps.java
@@ -14,6 +14,7 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class MatrixSteps {
+    //dec
     double det ;
     Matrix transposeMatrix ;
     Matrix inverseMatrix ;
